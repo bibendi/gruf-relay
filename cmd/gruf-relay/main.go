20f62c4a092856c20e3ce0dd8621256ac858c5ce
Misha Merkushin <merkushin.m.s@gmail.com>
2025-04-05 22:49:12 +0300

    feat: add cleanenv

--- cmd/gruf-relay/main.go
+++ cmd/gruf-relay/main.go
@@ -18,7 +18,6 @@ import (
 )
 
 // TODO:
-// - cleanenv
 // - slog
 // - Testify
 // - Metrics
--- internal/config/config.go
+++ internal/config/config.go
@@ -3,38 +3,29 @@ package config
 import (
 	"fmt"
 	"log"
-	"os"
 	"time"
 
-	"gopkg.in/yaml.v2"
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
-	Host                string        `yaml:"host" env-default:"0.0.0.0"`
-	Port                int           `yaml:"port" env-default:"8080"`
-	HealthCheckInterval time.Duration `yaml:"health_check_interval"`
-	Workers             WorkersConfig `yaml:"workers"`
-}
-
-type WorkersConfig struct {
-	Count     int `yaml:"count" env-default:"2"`
-	StartPort int `yaml:"start_port" env-default:"9000"`
+	Host                string        `yaml:"host" env:"HOST" env-default:"0.0.0.0"`
+	Port                int           `yaml:"port" env:"PORT" env-default:"8080"`
+	HealthCheckInterval time.Duration `yaml:"health_check_interval" env:"HEALTH_CHECK_INTERVAL" env-default:"5s"`
+	Workers             struct {
+		Count     int `yaml:"count" env:"WORKERS_COUNT" env-default:"2"`
+		StartPort int `yaml:"start_port" env:"WORKERS_START_PORT" env-default:"9000"`
+	}
 }
 
 func LoadConfig(filename string) (*Config, error) {
-	yamlFile, err := os.ReadFile(filename)
-	if err != nil {
+	var config Config
+
+	if err := cleanenv.ReadConfig(filename, &config); err != nil {
 		return nil, fmt.Errorf("failed to read config file: %w", err)
 	}
 
-	var config Config
-	config.setDefaultValues()
-	err = yaml.Unmarshal(yamlFile, &config)
-	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshal config: %w", err)
-	}
-
-	if err := validateConfig(&config); err != nil {
+	if err := config.validateConfig(); err != nil {
 		return nil, fmt.Errorf("invalid config: %w", err)
 	}
 
@@ -43,32 +34,20 @@ func LoadConfig(filename string) (*Config, error) {
 	return &config, nil
 }
 
-func (c *Config) setDefaultValues() {
-	if c.HealthCheckInterval == 0 {
-		c.HealthCheckInterval = 5 * time.Second
-	}
-	if c.Workers.Count == 0 {
-		c.Workers.Count = 2
-	}
-	if c.Workers.StartPort == 0 {
-		c.Workers.StartPort = 9000
-	}
-}
-
-func validateConfig(cfg *Config) error {
-	if cfg.Port <= 0 {
+func (c *Config) validateConfig() error {
+	if c.Port <= 0 {
 		return fmt.Errorf("port must be a positive integer")
 	}
 
-	if cfg.HealthCheckInterval <= 0 {
+	if c.HealthCheckInterval <= 0 {
 		return fmt.Errorf("health_check_interval must be a positive duration")
 	}
 
-	if cfg.Workers.Count <= 0 {
+	if c.Workers.Count <= 0 {
 		return fmt.Errorf("workers count must be a positive integer")
 	}
 
-	if cfg.Workers.StartPort <= 0 {
+	if c.Workers.StartPort <= 0 {
 		return fmt.Errorf("workers start_port must be a positive integer")
 	}
 
